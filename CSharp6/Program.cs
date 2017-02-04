8e88ba6596057f1e7f2984c5355083fd6b3360b8
Ashraf Ali <ashraf.ali1811@gmail.com>
2017-02-04 16:47:30 +0500

    Add project files.

---
+++ CSharp6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+namespace CSharp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Show();
+
+            ReadKey();
+        }
+
+
+        public static void Show() => Console.WriteLine($"{Assembly.GetExecutingAssembly().FullName}");
+ 
+    }
+
+
+}
